47a7905fecdd21333853034fd3431c8d5aef05a6
Asaduzzaman Rahim <rahim.asaduzzaman@gmail.com>
2026-04-20 05:14:14 +0600

    Benar Part Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navber from './Components/Navber'
+import Benar from "./Components/Benar"
 
 const App = () => {
   return (
     <div>
       <Navber/>
+      <Benar/>
     </div>
   )
 }
--- src/Components/Benar.jsx
+++ src/Components/Benar.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
 import Container from "./Container"
+import Button from './Button'
+import BenarPhoto from "../assets/Benar.jpg"
 
 const Benar = () => {
   return (
     <>
      <section className='pt-32 pb-56'>
         <Container>
-            <div>
+            <div className='flex flex-wrap-reverse justify-between gap-3.5 lg:flex-nowrap '>
+                <div className='max-w-[648px] flex flex-col justify-between gap-5'>
+                    <div className='space-y-6'>
+                      <h1 className='text-7xl font-semibold leading-[84px] text-gray9'>Hello, I’m Brooklyn Gilbert</h1>
+                      <p>I'm a Freelance <span className='text-gray9'>UI/UX Designer</span> and <span className='text-gray9'>Developer</span> based in London, England. I strives to build immersive and beautiful web applications through carefully crafted code and user-centric design.</p>
+                      <Button>Say Hello!</Button>
+                    </div>
+                    <div className="flex flex-wrap gap-3 w-full">
+                      <div className="text-center md:w-[200px] max-w-[200px] py-4 px-2 bg-[#f6ebfe]">
+                        <h3 className='text-[32px] text-gray7 font-semibold leading-10'>15 Y.</h3>
+                        <p>Experience</p>
+                      </div>
+                      <div className="text-center md:w-[200px] max-w-[200px] py-4 px-2 bg-[#f6ebfe]">
+                        <h3 className='text-[32px] text-gray7 font-semibold leading-10'>15 Y.</h3>
+                        <p>Experience</p>
+                      </div>
+                      <div className="text-center md:w-[200px] max-w-[200px] py-4 px-2 bg-[#f6ebfe]">
+                        <h3 className='text-[32px] text-gray7 font-semibold leading-10'>15 Y.</h3>
+                        <p>Experience</p>
+                      </div>
+                    </div>
+                </div>
                 <div>
-                    
+                  <img src={BenarPhoto} alt="Benar Photo" />
                 </div>
             </div>
         </Container>

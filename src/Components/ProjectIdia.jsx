855b362fbcb244d13ac90f7a8de6b3b8675f3a76
Asaduzzaman Rahim <rahim.asaduzzaman@gmail.com>
2026-04-24 16:49:12 +0600

    Completed to projectIdia Section

--- src/Components/ProjectIdia.jsx
+++ src/Components/ProjectIdia.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Container from "./Container"
 import Section from "./Section"
+import Button from "./Button"
+
+import { FaArrowRight } from "react-icons/fa6";
 
 
 const ProjectIdia = () => {
@@ -8,8 +11,12 @@ const ProjectIdia = () => {
     <>
      <Section className="bg-gray9">
         <Container>
-            <div className="max-w-[619px] mx-auto">
-
+            <div className="max-w-[619px] mx-auto space-y-8 text-center">
+                <h3 className='text-5xl leading-14 font-semibold text-white'>Do you have Project Idia?. Let's discuss your project!</h3>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
+                <div className='flex justify-center'>
+                  <Button className="flex gap-3 items-center">Let`s Work Together <FaArrowRight/></Button>
+                </div>
             </div>    
         </Container>    
     </Section> 
